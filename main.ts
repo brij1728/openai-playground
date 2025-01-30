c3e6339aa3c252625ea9c6a72b37a7ebad14fa60
Brijesh Kumar <bk10895@gmail.com>
2025-01-31 00:07:37 +0530

    initialize project with basic structure and OpenAI integration

---
+++ main.ts
@@ -0,0 +1,29 @@
+import { TextLineStream } from "@std/streams";
+import { toTransformStream } from "@std/streams/to-transform-stream";
+
+const response = await fetch("https://example.com/data.txt");
+
+if(response.body) {
+  const transformedStream = response.body
+  .pipeThrough(new TextDecoderStream())
+  .pipeThrough(new TextLineStream())
+  .pipeThrough(toTransformStream( async function* (src) {
+    for await (const chunk of src) {
+      
+      yield chunk;
+    }
+  }));
+  const reader = transformedStream.getReader();
+  
+  while (true) {
+    const { done, value } = await reader.read();
+    if (done) {
+      break;
+    }
+    console.log(value);
+    
+  
+  
+  
+  }
+}
